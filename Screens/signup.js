73af8051e6c048a8e2a0095debad3ae3991f7f74
anjummariya <70070324+anjummariya@users.noreply.github.com>
2023-05-08 17:36:19 +0530

    checkbox update

--- Screens/signup.js
+++ Screens/signup.js
@@ -1,64 +1,76 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function SignupPage({navigation}) {
+export default function SignupPage({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isDonor, setIsDonor] = useState(false);
 
   function handleSignup() {
-    console.log(`Name: ${name}\nEmail: ${email}\nPassword: ${password}`);
+    console.log(`Name: ${name}\nEmail: ${email}\nPassword: ${password}\nIs Donor: ${isDonor}`);
   }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity  onPress={() => navigation.navigate('Entry')} style={{paddingLeft:10, paddingTop:25,}}>
-            <Ionicons name="arrow-back" size={30} color="white" />
+      <TouchableOpacity onPress={() => navigation.navigate('Entry')} style={{ paddingLeft: 10, paddingTop: 25, }}>
+        <Ionicons name="arrow-back" size={30} color="white" />
       </TouchableOpacity>
       <View style={styles.innercontent}>
-      <View style={styles.content}>
-      <Text style={styles.heading}>Sign Up</Text>
+        <View style={styles.content}>
+          <Text style={styles.heading}>Sign Up</Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+        />
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity
+            style={styles.checkbox}
+            onPress={() => setIsDonor(!isDonor)}
+          >
+            {isDonor ? (
+              <Ionicons name="checkbox-outline" size={24} color="black" />
+            ) : (
+              <Ionicons name="square-outline" size={24} color="black" />
+            )}
+            <Text style={styles.checkboxLabel}>Register as a donor?</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleSignup}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        onChangeText={(text) => setName(text)}
-        value={name}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        secureTextEntry={true}
-      />
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}>
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
     </View>
-    </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#da3e56',
     justifyContent: 'center',
-    
   },
-  innercontent:{
-    flex:1,
-    alignItems:'center',
+  innercontent: {
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
   heading: {
@@ -74,6 +86,20 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderRadius: 5,
     fontSize: 15,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  checkboxLabel: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: 'white',
   },
   button: {
     backgroundColor: 'white',
